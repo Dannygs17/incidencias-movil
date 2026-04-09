b0c91bdeb577e39f4b5501e701020d0936b93cd3
Daniel <dannysantes17@gmail.com>
2026-04-09 12:42:16 -0600

    version lista para pruebas con produccion2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  // 1. AGREGA ESTA LÍNEA CON TU DOMINIO REAL (Asegúrate que termine en /api)
+  apiUrl: 'https://incidenciassmart.site/api',
+  firebase: {
+    apiKey: "AIzaSyBdnbmieMbw25bR0FxsrdL8mXRW80w-nzE",
+    authDomain: "conexionfirebase-2b4a9.firebaseapp.com",
+    projectId: "conexionfirebase-2b4a9",
+    storageBucket: "conexionfirebase-2b4a9.firebasestorage.app",
+    messagingSenderId: "861526039202",
+    appId: "1:861526039202:web:85968a55199ce8f779d5fb",
+    measurementId: "G-K0ZE680ZE2"
+  }
 };
